34d2bd44611023a8bbdc11a47e3587a49b6a3595
koo <rnwnsgur28@gmail.com>
2024-11-18 16:21:36 +0900

    feat: Home  상단바 구현

---
+++ src/components/HomeTitle.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import dgm_logo from "../assets/images/dgm_logo.png";
+import bi_logo from "../assets/images/bi_logo.png";
+
+const HomeTitle = () => {
+  return (
+    <TitleContainer>
+      <LogoImage src={dgm_logo} alt="DGM Logo" />
+      <TitleText>HOME</TitleText>
+      <LogoImage src={bi_logo} alt="BI Logo" />
+    </TitleContainer>
+  );
+};
+
+export default HomeTitle;
+
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100px;
+  border-bottom: 1px solid #fff;
+  padding: 20px 0px;
+`;
+
+const LogoImage = styled.img`
+  height: 60px;
+  width: auto;
+`;
+
+const TitleText = styled.div`
+  display: flex;
+  font-size: 60px;
+`;
