e0568fea0a04566673f30c5749a70290c85775c4
koo <rnwnsgur28@gmail.com>
2024-11-18 16:24:00 +0900

    style: 컨테이너 정렬 삭제

--- src/styles/Container.js
+++ src/styles/Container.js
@@ -2,8 +2,6 @@ import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
   height: 100vh;
   width: 100%;
   max-width: 1200px;
