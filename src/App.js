03b397c053234bd0ba8e29ebbecd76f500688f2b
koo <rnwnsgur28@gmail.com>
2024-11-23 21:18:44 +0900

    feat: 미등록자 인식 기록 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import GlobalStyle from "./styles/GlobalStyle";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Unknown from "./pages/Unknown";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/unknown" element={<Unknown />} />
         </Routes>
       </Router>
       <div></div>
