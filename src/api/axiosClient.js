4a65c332914399160d2362440d242e21c6d79f6c
koo <rnwnsgur28@gmail.com>
2024-11-25 01:17:23 +0900

    feat: 배포된 백엔드와 연결

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8080", // Spring Boot 서버 주소
+  baseURL: "http://52.78.196.254", // Spring Boot 서버 주소
   headers: {
     "Content-Type": "application/json",
   },
